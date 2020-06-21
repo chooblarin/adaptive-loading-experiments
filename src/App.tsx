a0219528150f0d448e24357b9c6ffd0bd3942897
Sota Hatakeyama <choo.bla.rin@gmail.com>
2020-06-21 23:09:48 +0900

    Demo network status

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { useNetworkStatus } from "react-adaptive-hooks";
 import "./App.css";
 
 function App() {
+  const { effectiveConnectionType } = useNetworkStatus();
+
   return (
     <div className="App">
       <h1>Adaptive loading experiments</h1>
+      <div>{effectiveConnectionType}</div>
     </div>
   );
 }
