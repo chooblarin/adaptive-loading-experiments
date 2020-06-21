6e23debf63ed86babe611f6aa1913aeac748e734
Sota Hatakeyama <choo.bla.rin@gmail.com>
2020-06-21 23:32:42 +0900

    Demo the rest...

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import React from "react";
-import { useNetworkStatus } from "react-adaptive-hooks";
+import { useHardwareConcurrency } from "react-adaptive-hooks/hardware-concurrency";
+import { useMemoryStatus } from "react-adaptive-hooks/memory";
+import { useNetworkStatus } from "react-adaptive-hooks/network";
+import { useSaveData } from "react-adaptive-hooks/save-data";
 import "./App.css";
 
 function App() {
   const { effectiveConnectionType } = useNetworkStatus();
-
+  const { saveData } = useSaveData();
+  const hardwareConcurrency = useHardwareConcurrency();
+  const { deviceMemory } = useMemoryStatus();
   return (
     <div className="App">
       <h1>Adaptive loading experiments</h1>
-      <div>{effectiveConnectionType}</div>
+      <div>
+        Network: <span>{effectiveConnectionType}</span>
+      </div>
+      <div>
+        Save Data: <span>{String(saveData)}</span>
+      </div>
+      <div>
+        CPU Cores:
+        <span>
+          {hardwareConcurrency.unsupported
+            ? "Unsupported"
+            : hardwareConcurrency.numberOfLogicalProcessors}
+        </span>
+      </div>
+      <div>
+        Memory: <span>{String(deviceMemory)}</span>
+      </div>
     </div>
   );
 }
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,6 @@
-declare module "react-adaptive-hooks" {
+// Referenced from https://github.com/GoogleChromeLabs/react-adaptive-hooks/pull/21
+
+declare module "react-adaptive-hooks/network" {
   export type EffectiveConnectionType = "slow-2g" | "2g" | "3g" | "4g";
   export function useNetworkStatus(
     initialEffectiveConnectionType?: EffectiveConnectionType | null
@@ -6,4 +8,32 @@ declare module "react-adaptive-hooks" {
     unsupported: boolean;
     effectiveConnectionType: EffectiveConnectionType | null;
   };
+}
+
+declare module "react-adaptive-hooks/save-data" {
+  export function useSaveData(
+    initialSaveDataStatus?: boolean | null
+  ): { unsupported: boolean; saveData: boolean | null };
+}
+
+declare module "react-adaptive-hooks/hardware-concurrency" {
+  export function useHardwareConcurrency():
+    | { unsupported: true }
+    | { unsupported: false; numberOfLogicalProcessors: number };
+}
+
+declare module "react-adaptive-hooks/memory" {
+  export interface MemoryStatus {
+    deviceMemory: number;
+    totalJSHeapSize: number | null;
+    usedJSHeapSize: number | null;
+    jsHeapSizeLimit: number | null;
+  }
+  export function useMemoryStatus(
+    initialMemoryStatus?: MemoryStatus
+  ):
+    | ({ unsupported: true } & Partial<MemoryStatus>)
+    | ({
+        unsupported: false;
+      } & MemoryStatus);
 }
